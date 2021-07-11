d2099193be1a76122f3ce95cbdfed5d97300612e
DMinghao <minghaodu96@gmail.com>
2021-07-11 16:47:28 -0400

    Update reddit_data.R

--- src/reddit_data.R
+++ src/reddit_data.R
@@ -1,11 +1,17 @@
 if(!require(RedditExtractoR)) {
   install.packages("RedditExtractoR")
 }
-if(!require(tidytext)) {
+if (!require(tidytext)) {
   install.packages("tidytext")
 }
-if(!require(textdata)) {
+if (!require(textdata)) {
   install.packages("textdata")
+}
+if (!require(RTextTools)) {
+  install.packages("RTextTools")
+}
+if (!require(ggridges)) {
+  install.packages("ggridges")
 }
 library(RedditExtractoR)
 library(plotly)
@@ -13,39 +19,33 @@ library(tidyverse)
 library(tm)
 library(tidytext)
 library(textdata)
+library(RTextTools)
+library(ggplot2)
+library(ggridges)
 
-getRedditData <- function(keyword, subreddit = NA, sort_by = "comments") {
+getRedditData <- function(keyword,
+                          subreddit = NA,
+                          sort_by = "comments") {
   if (length(keyword) > 1) {
     return(NULL)
   }
   
-  page_threshold <- 0
-  cn_threshold <- 0
-  
-  sort_by %>% switch ("new" = {
-    page_threshold <<- 10
-    cn_threshold <<- 0
-  },
-  "comments" = {
-    page_threshold <<- 1
-    cn_threshold <<- 50
-  },
-  "relevance" = {
-    page_threshold <<- 5
-    cn_threshold <<- 10
-  })
+  page_threshold <- sort_by %>%
+    switch ("new" = 3,
+            "comments" = 1,
+            "relevance" = 1)
   
   get_reddit(
     search_terms = keyword,
     regex_filter = "",
     subreddit = subreddit,
-    cn_threshold = cn_threshold,
+    cn_threshold = 0,
     page_threshold = page_threshold,
     sort_by = sort_by
   )
 }
 
-keyword <- "covid"
+keyword <- "trump"
 
 data <- getRedditData(keyword)
 
@@ -53,7 +53,7 @@ data <- getRedditData(keyword)
 # get_sentiments("bing")
 # get_sentiments("afinn")
 # get_sentiments("loughran")
-wordDict <- get_sentiments("nrc") 
+wordDict <- get_sentiments("nrc")
 
 dictFactor <- wordDict %>% mutate(sentiment = as.factor(sentiment))
 
@@ -67,50 +67,171 @@ copyData <- copyData %>%
   mutate(post_date = as.Date(post_date, "%d-%m-%y")) %>%
   mutate(comm_date = as.Date(comm_date, "%d-%m-%y")) %>%
   mutate(subreddit = as.factor(subreddit)) %>%
-as_tibble()
+  mutate(uniqueID = row_number()) %>%
+  as_tibble()
 
 summary(copyData)
 glimpse(copyData)
 
+
+
+
+### user network
+
 copyData %>%
-  select(link) %>%
+  select(title) %>%
   unique() -> uniqueThreads
 
 userNetPlot <- copyData %>%
-  filter(link == uniqueThreads[1,]) %>%
+  filter(title == uniqueThreads[1,]) %>%
   user_network(agg = TRUE)
 
-userNetPlot$plot
+# userNetPlot$plot
 
 
-fig <- copyData %>% plot_ly(
-  x = ~ post_score,
-  y = ~ num_comments,
-  z = ~ comment_score,
-  color = ~ upvote_prop,
-  hovertemplate = ~ paste(
-    "<br>post_score: ",
-    post_score,
-    "<br>num_comments: ",
-    num_comments,
-    "<br>comment_score: ",
-    comment_score,
-    "<br>upvote_prop: ",
-    upvote_prop,
-    "<br>controversiality: ",
-    controversiality,
-    "<br>---Comment---<br>",
-    comment,
-    '<extra></extra>'
-  )
-) %>% add_markers() %>%
+
+
+
+### 3d scatter
+
+fig <- copyData %>%
+  # filter(controversiality > 0) %>%
+  plot_ly(
+    x = ~ post_score,
+    y = ~ num_comments,
+    z = ~ comment_score,
+    color = ~ upvote_prop,
+    hovertemplate = ~ paste(
+      "<br>post_score: ",
+      post_score,
+      "<br>num_comments: ",
+      num_comments,
+      "<br>comment_score: ",
+      comment_score,
+      "<br>upvote_prop: ",
+      upvote_prop,
+      "<br>controversiality: ",
+      controversiality,
+      "<br>---Comment---<br>",
+      comment,
+      '<extra></extra>'
+    )
+  ) %>% add_markers() %>%
   layout(scene = list(
     camera = list(eye = list(
-      x = 1.87, y = 0.88, z = 0.64
+      x = 3, y = 0.88, z = 0.64
     )),
     aspectratio = list(x = 1, y = 1, z = 1.75)
   ))
 fig
+
+
+
+
+
+### Text mining tf, idf
+
+data("stop_words")
+textData <- copyData %>%
+  select(c(uniqueID, comment, title, post_text)) %>%
+  unite(allText, -1) %>%
+  mutate(allText = gsub(allText, pattern = "http\\S+\\s*", replacement = "")) %>%
+  mutate(allText = gsub(allText, pattern = "<.*?>", replacement = "")) %>%
+  mutate(allText = gsub(allText, pattern = "[0-9]+|[[:punct:]]|\\(.*\\)", replacement = ""))
+
+textData %>%
+  unnest_tokens(input = allText, output = word) %>%
+  anti_join(stop_words) -> cleanStemmedText
+
+cleanStemmedText %>%
+  count(word) %>%
+  arrange(desc(n))
+
+cleanStemmedText %>%
+  group_by(uniqueID) %>%
+  count(word) %>%
+  ungroup() -> countTbl
+
+tfidfTbl <- countTbl %>%
+  bind_tf_idf(word, document = uniqueID , n)
+
+tfidfTbl <- tfidfTbl %>%
+  merge(copyData %>% select(c(uniqueID, upvote_prop)), all = TRUE) %>%
+  as_tibble()
+
+tfidfFig <- tfidfTbl %>%
+  filter(n > 2) %>%
+  plot_ly(
+    x = ~ tf,
+    y = ~ idf,
+    z = ~ tf_idf,
+    color = ~ upvote_prop,
+    hovertemplate = ~ paste(
+      "<br>uniqueID: ",
+      uniqueID,
+      "<br>tf: ",
+      tf,
+      "<br>idf: ",
+      idf,
+      "<br>tf_idf: ",
+      tf_idf,
+      "<br>n: ",
+      n,
+      "<br>upvote_prop: ",
+      upvote_prop,
+      "<br>word: ",
+      word,
+      '<extra></extra>'
+    )
+  ) %>%
+  add_markers() %>%
+  layout(scene = list(camera = list(eye = list(
+    x = -2, y = 0.88, z = 1
+  ))))
+tfidfFig
+
+
+
+
+### sentiment
+###
+
+sentimentData <- copyData %>%
+  select(c(uniqueID, post_date, comm_date)) %>%
+  merge(cleanStemmedText, all = TRUE) %>%
+  inner_join(get_sentiments("nrc"), by = "word") %>%
+  as_tibble()
+
+glimpse(sentimentData)
+
+sentimentData %>%
+  group_by(sentiment) %>%
+  tally() %>%
+  arrange(desc(n))
+
+sentimentData %>% group_by(comm_date) %>% count()
+
+p <- sentimentData %>%
+  ggplot() +
+  geom_joy(
+    aes(x = comm_date, y = sentiment, fill = sentiment),
+    rel_min_height = 0.01,
+    alpha = 0.7,
+    scale = 3
+  ) +
+  theme_joy() +
+  scale_fill_discrete(guide = FALSE)
+
+p
+
+sentimentData %>%
+  group_by(uniqueID, sentiment) %>%
+  tally() %>%
+  # group_by(uniqueID) %>% filter(n == max(n)) %>%
+  
+  plot_ly(type = 'scatterpolar',
+          theta = ~ sentiment,
+          r = ~ n)
 
 
 
