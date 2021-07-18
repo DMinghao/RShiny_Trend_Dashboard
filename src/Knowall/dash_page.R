64cd55a19b8c4b7df87a388b26d16e0ac239c881
DMinghao <45401748+DMinghao@users.noreply.github.com>
2021-07-18 19:12:34 -0400

    dash infrastructure

--- src/Knowall/dash_page.R
+++ src/Knowall/dash_page.R
@@ -1,4 +1,33 @@
-dash_page <- div(h1("Dashboard page"), 
-                   switchInput("demoMode", label = "DEMO", value = TRUE),
-                 textOutput("keywordState"), 
-                 )
+dash_page <- div(
+  h1("Dashboard page"),
+  switchInput("demoMode", label = "DEMO", value = TRUE),
+  conditionalPanel(
+    condition = "input.demoMode == false",
+    textInput(
+      inputId = "keywords",
+      label = "Enter your Keyword",
+      placeholder = "keywords seperated by ',' (Max 2)",
+      width = "450px"
+    )
+  ),
+  conditionalPanel(
+    condition = "input.demoMode == true",
+    selectizeInput(
+      inputId = "keywordSelect",
+      label = "Select demo keywords",
+      choices = c("a", "aa", "b", "bb"),
+      selected = NULL,
+      multiple = TRUE,
+      options = list(maxItems = 2)
+    )
+  ),
+  actionButton("go", "Go"), 
+  textOutput("keywordState"), 
+  div(id = "outputDiv", 
+      tabsetPanel(
+        tabPanel("Google Trends"), 
+        tabPanel("Twitter"), 
+        tabPanel("Reddit")
+      )
+      )
+)
--- src/Knowall/server.R
+++ src/Knowall/server.R
@@ -1,24 +1,61 @@
-
-
 if (!require(shiny.router)) {
   install.packages("shiny.router")
 }
+if (!require(stringr)) {
+  install.packages("stringr")
+}
 library(shiny)
+library(stringr)
 
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   router$server(input, output, session)
   
-  global <- reactiveValues(
-    keywordProvided = FALSE
+  global <-
+    reactiveValues(
+      keywordProvided = c(),
+      displayKeyword = "",
+      GoogleData = NULL,
+      TwitterData1 = NULL,
+      RedditData1 = NULL,
+      TwitterData2 = NULL,
+      RedditData2 = NULL
     )
   
-  output$keywordState <-
-    renderText(if (global$keywordProvided) {
-      input$keywords
+  observeEvent(input$go, {
+    global$displayKeyword <-  updateKeywordText()
+  })
+  
+  updateKeywordText <- function() {
+    if (input$demoMode == TRUE) {
+      global$keywordProvided = input$keywordSelect
+      # print(input$keywordSelect)
+    } else {
+      temp = str_split(input$keywords, ',', n = 2)[[1]]
+      # print(temp)
+      global$keywordProvided = temp
+    }
+    rt <- "Not Provided"
+    if (length(global$keywordProvided) > 0) {
+      rt = paste(global$keywordProvided, ", ")
+    }
+    rt
+  }
+  
+  getData <- function() {
+    if(input$demoMode == TRUE){
+      
     }else {
-      "Not Provided"
-    }) 
+      if(length(global$keywordProvided) == 2 && !("" %in% global$keywordProvided)){
+        
+      }
+    }
+  }
+  
+  output$keywordState <- renderText({
+    global$displayKeyword
+  })
+  
   
 })
